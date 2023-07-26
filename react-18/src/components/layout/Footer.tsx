93d724b95191ba24981ef66f4bbf1ff39f7fdf59
shakya <shakya980528@gmail.com>
2023-07-26 03:31:22 -0700

    Adding footer styles

--- react-18/src/components/layout/Footer.tsx
+++ react-18/src/components/layout/Footer.tsx
@@ -3,7 +3,25 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <p>Footer</p>
+      <div className="row ml-4 mr-4 footer-inner">
+        <div className="col-md-4 mb-1">
+          <p className="text-color-1">Version 1.0</p>
+        </div>
+        <div className="col-md-4 mb-1"></div>
+        <div className="col-md-4 mb-1">
+          <div className="row">
+            <div className="col-md-4 mb-1 right-align">
+              <p className="text-3 text-color-2">About</p>
+            </div>
+            <div className="col-md-4 mb-1 right-align">
+              <p className="text-3 text-color-2">Privecy</p>
+            </div>
+            <div className="col-md-4 mb-1 right-align">
+              <p className="text-3 text-color-2">Terms</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
